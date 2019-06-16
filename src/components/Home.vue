1b6bf48d73066f53e6b0b2165624181c9162a93e
nifan <nifan1950333426@qq.com>
2019-06-16 22:22:34 +0800

    完成头部

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>首页</div>
+  <div>
+    <home-header></home-header>
+  </div>
 </template>
 
 <script>
+import homeHeader from './pages/home/header'
 export default {
   name: 'home',
+  components: {
+    homeHeader,
+  }
 }
 </script>
 
