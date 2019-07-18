9f0fbe860554fc767ba60c69be9e7ec03e4e2456
nifan <nifan1950333426@qq.com>
2019-07-18 21:21:50 +0800

    完成搜索歌曲播放功能

--- src/components/pages/searchSong/searchSong.vue
+++ src/components/pages/searchSong/searchSong.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="search-song">
-    <label>
-      <div class="search-container">
-        <div class="search-left iconfont icon-sousuo"></div>
-        <input class="search-right" v-model="query" placeholder="搜索歌曲、歌手、专辑" id="search" type="text" />
-      </div>
-    </label>
+    <form @submit.stop.prevent="handleQuery">
+      <label>
+        <div class="search-container">
+          <div class="search-left iconfont icon-sousuo"></div>
+          <input
+            class="search-right"
+            v-model="query"
+            placeholder="搜索歌曲、歌手、专辑"
+            id="search"
+            type="text"
+          />
+          <span v-show="query && query.length" @click.stop.prevent="handleDel" class="del iconfont icon-del2"></span>
+        </div>
+      </label>
+    </form>
     <div class="recommend-search border-top">
       <div class="title">热门搜索</div>
       <ul class="search-list">
@@ -14,36 +23,71 @@
           v-show="index < 9"
           v-for="(song,index) of hotkey"
           :key="song.n"
+          @click="handleHotClick(song.k)"
         >{{song.k}}</li>
       </ul>
+    </div>
+
+    <div class="searchSong" v-show="searchList && searchList.length">
+      <ol id="songs" class="list">
+        <li
+          @click="handleSongClick(song)"
+          class="new-song"
+          v-for="song of searchList"
+          :key="song.id"
+        >
+          <div class="song-name">{{song.songname}}</div>
+          <div class="song-text">
+            <span class="iconfont icon-sq"></span>
+            <span class="song-author">{{song.singer[0].name}}{{song.subtitle}}</span>
+          </div>
+          <span class="iconfont icon-bofanganniu play-icon-wrapper"></span>
+        </li>
+      </ol>
     </div>
   </div>
 </template>
 
 <script>
-import { getSearchList,getSearchSong } from "common/js/getSearchList";
+import { getSearchList, getSearchSong } from "common/js/getSearchList";
 export default {
   watch: {
     query() {
-      this._getSearchSong()
+      if (!this.query) {
+        this.searchList = [];
+      }
     }
   },
   data() {
     return {
       hotkey: [],
-      query: ""
+      query: "",
+      searchList: []
     };
   },
   methods: {
+    handleDel() {
+      this.query = "";
+    },
+    handleHotClick(name) {
+      this.query = name;
+    },
+    handleSongClick(song) {
+      this.$store.commit("isShow", true);
+      this.$store.commit("addSong", song);
+    },
+    handleQuery() {
+      this._getSearchSong();
+    },
     _getSearchList() {
       getSearchList().then(res => {
         this.hotkey = res.data.data.hotkey;
       });
     },
     _getSearchSong() {
-      getSearchSong(this.query).then((res) => {
-        console.log(res)
-      })
+      getSearchSong(this.query).then(res => {
+        this.searchList = res.data.data.song.list;
+      });
     }
   },
   created() {
@@ -53,14 +97,25 @@ export default {
 </script>
 
 <style scoped>
+.del {
+  position: absolute;
+  right: 3px;
+  top: 50%;
+  transform: translateY(-50%);
+  font-size: 20px;
+  padding: 5px;
+}
+
 .search-song {
   position: absolute;
   top: 105px;
   left: 0;
   width: 100%;
+  height: 100vh;
 }
 
 .search-container {
+  position: relative;
   display: flex;
   height: 30px;
   margin: 15px 10px;
@@ -100,6 +155,60 @@ export default {
   color: #333;
   border: 1px solid #ededed;
   border-radius: 30px;
+}
+
+.searchSong {
+  position: absolute;
+  top: 60px;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background: #fff;
+}
+
+.song-name {
+  margin-top: 5px;
+  font-size: 17px;
+}
+
+.song-text {
+  margin-top: 5px;
+  line-height: 17px;
+  font-size: 0;
+}
+
+.icon-sq {
+  vertical-align: middle;
+  margin-right: 3px;
+  color: #d43c33;
+}
+
+.song-author {
+  vertical-align: middle;
+  font-size: 12px;
+  color: #888;
+}
+
+.new-song {
+  position: relative;
+  margin-left: 10px;
+  padding-bottom: 6px;
+  padding-top: 5px;
+  border-bottom: 1px solid #e2e2e3;
+}
+
+.play-icon-wrapper {
+  position: absolute;
+  display: block;
+  font-size: 22px;
+  right: 8px;
+  top: 50%;
+  transform: translateY(-50%);
+  color: #888;
+}
+
+.list .icon-sq {
+  fill: #fe672e;
 }
 </style>
 
